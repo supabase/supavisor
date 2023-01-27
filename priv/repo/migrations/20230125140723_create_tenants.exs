2e6e4fb64470ca91d12a3d51fe864b7582e5a318
abc3 <sts@abc3.dev>
2023-01-27 13:28:14 +0200

    Update priv/repo/migrations/20230125140723_create_tenants.exs
    Co-authored-by: Wojtek Mach <wojtekmach@users.noreply.github.com>

--- priv/repo/migrations/20230125140723_create_tenants.exs
+++ priv/repo/migrations/20230125140723_create_tenants.exs
@@ -4,13 +4,13 @@ defmodule PgEdge.Repo.Migrations.CreateTenants do
   def change do
     create table(:tenants, primary_key: false, prefix: "pgedge") do
       add(:id, :binary_id, primary_key: true)
-      add(:external_id, :string)
-      add(:db_host, :string)
-      add(:db_port, :integer)
-      add(:db_user, :string)
-      add(:db_database, :string)
-      add(:db_password, :string)
-      add(:pool_size, :integer)
+      add(:external_id, :string, null: false)
+      add(:db_host, :string, null: false)
+      add(:db_port, :integer, null: false)
+      add(:db_user, :string, null: false)
+      add(:db_database, :string, null: false)
+      add(:db_password, :string, null: false)
+      add(:pool_size, :integer, null: false)
 
       timestamps()
     end
