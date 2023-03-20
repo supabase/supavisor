aac199bd4576eb9a8cbed20a3bbc7c35d471c93b
abc3 <sts@abc3.dev>
2023-03-20 15:51:19 +0100

    cluster tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,10 @@
+Node.start(:"primary@127.0.0.1", :longnames)
+{:ok, _pid, node} = :peer.start(%{name: :"secondary@127.0.0.1"})
+true = :erpc.call(node, :code, :set_path, [:code.get_path()])
+
+Supavisor.Support.Cluster.apply_config(node)
+
+{:ok, _} = :erpc.call(node, :application, :ensure_all_started, [:supavisor])
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Supavisor.Repo, :manual)
