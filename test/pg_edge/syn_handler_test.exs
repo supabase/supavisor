dd50f3093624bdddd806945ff6826e3806d656c3
abc3 <sts@abc3.dev>
2023-03-22 12:29:31 +0100

    tenant for syn test

--- test/pg_edge/syn_handler_test.exs
+++ test/pg_edge/syn_handler_test.exs
@@ -4,18 +4,17 @@ defmodule Supavisor.SynHandlerTest do
   require Logger
   alias Ecto.Adapters.SQL.Sandbox
 
-  @tenant "proxy_tenant"
+  @tenant "syn_tenant"
 
   test "resolving conflict" do
     node2 = :"secondary@127.0.0.1"
 
     {:ok, pid2} = :erpc.call(node2, Supavisor, :start, [@tenant])
-    Process.sleep(2_500)
-    IO.inspect({pid2, Supavisor.get_global_sup(@tenant)})
+    Process.sleep(500)
     assert pid2 == Supavisor.get_global_sup(@tenant)
     assert node(pid2) == node2
     true = Node.disconnect(node2)
-    Process.sleep(2_500)
+    Process.sleep(500)
 
     assert nil == Supavisor.get_global_sup(@tenant)
     {:ok, pid1} = Supavisor.start(@tenant)
@@ -23,9 +22,9 @@ defmodule Supavisor.SynHandlerTest do
     assert node(pid1) == node()
 
     :pong = Node.ping(node2)
-    Process.sleep(2_500)
+    Process.sleep(500)
 
-    msg = "Resolving proxy_tenant conflict, stop local pid"
+    msg = "Resolving syn_tenant conflict, stop local pid"
 
     assert capture_log(fn -> Logger.warn(msg) end) =~
              msg
