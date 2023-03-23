d1f9aeee4978bc84a59274e23e112c1f522de267
abc3 <sts@abc3.dev>
2023-03-23 10:11:33 +0100

    update test description

--- test/supavisor/jwt_test.exs
+++ test/supavisor/jwt_test.exs
@@ -6,17 +6,17 @@ defmodule Supavisor.JwtTest do
   @wrong_secret "my_wrong_secret_key"
 
   describe "authorize/2" do
-    test "returns {:ok, claims} for a valid token" do
+    test "returns claims for a valid token" do
       {:ok, token, _} = create_valid_jwt_token()
       assert {:ok, claims} = Jwt.authorize(token, @secret)
       assert claims["role"] == "test"
     end
 
-    test "returns {:error, :token_not_a_string} for non-binary token" do
+    test "returns token_not_a_string for non-binary token" do
       assert {:error, :token_not_a_string} = Jwt.authorize(123, @secret)
     end
 
-    test "returns {:error, :signature_error} for a wrong secret" do
+    test "returns signature_error for a wrong secret" do
       {:ok, token, _} = create_valid_jwt_token()
       assert {:error, :signature_error} = Jwt.authorize(token, @wrong_secret)
     end
