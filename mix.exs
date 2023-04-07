537397dae5813d2e51b6fa5b5aa8cc76723ba610
abc3 <sts@abc3.dev>
2023-04-07 18:11:35 +0200

    v0.1.0 (#60)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Supavisor.MixProject do
   def project do
     [
       app: :supavisor,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
