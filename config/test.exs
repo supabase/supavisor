6aea10263bf4f146de6fb4b4d7b1a2d36a0f3078
RTLS <carter.e.pedersen@gmail.com>
2023-05-04 09:53:59 -0700

    Revert "🔧 Remove port from test.exs"
    This reverts commit 65688265bb36c5e18ce3fd2dc87b42a38f1fc032.

--- config/test.exs
+++ config/test.exs
@@ -17,7 +17,8 @@ config :supavisor, Supavisor.Repo,
   hostname: "localhost",
   database: "supavisor_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: 10,
+  port: 6432
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
