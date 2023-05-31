afde09102137f7e0d60a150fdb05b17d2916b192
joel@joellee.org <joel@joellee.org>
2023-05-31 23:26:12 +0800

    feat: initial commit with otel deps

--- mix.exs
+++ mix.exs
@@ -61,7 +61,13 @@ defmodule Supavisor.MixProject do
       # pooller
       {:poolboy, "~> 1.5.2"},
       {:syn, "~> 3.3"},
-      {:pgo, "~> 0.13"}
+      {:pgo, "~> 0.13"},
+
+      {:opentelemetry, "~> 1.3"},
+      {:opentelemetry_api, "~> 1.2"},
+      {:opentelemetry_exporter, "~> 1.4"},
+      {:opentelemetry_phoenix, "~> 1.1"},
+      {:opentelemetry_cowboy, "~> 0.2"}
       # TODO: add ranch deps
     ]
   end
