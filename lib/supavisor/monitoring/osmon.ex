f3f82d14b196dd2b7d9571e42dc5a709e9b39afe
abc3 <sts@abc3.dev>
2023-06-20 16:34:10 +0200

    chore: add prefix to osmon metrics

--- lib/supavisor/monitoring/osmon.ex
+++ lib/supavisor/monitoring/osmon.ex
@@ -9,6 +9,7 @@ defmodule Supavisor.PromEx.Plugins.OsMon do
   @event_ram_usage [:prom_ex, :plugin, :osmon, :ram_usage]
   @event_cpu_util [:prom_ex, :plugin, :osmon, :cpu_util]
   @event_cpu_la [:prom_ex, :plugin, :osmon, :cpu_avg1]
+  @prefix [:supavisor, :prom_ex]
 
   @impl true
   def polling_metrics(opts) do
@@ -26,32 +27,32 @@ defmodule Supavisor.PromEx.Plugins.OsMon do
       {__MODULE__, :execute_metrics, []},
       [
         last_value(
-          [:osmon, :ram_usage],
+          @prefix ++ [:osmon, :ram_usage],
           event_name: @event_ram_usage,
           description: "The total percentage usage of operative memory.",
           measurement: :ram
         ),
         last_value(
-          [:osmon, :cpu_util],
+          @prefix ++ [:osmon, :cpu_util],
           event_name: @event_cpu_util,
           description:
             "The sum of the percentage shares of the CPU cycles spent in all busy processor states in average on all CPUs.",
           measurement: :cpu
         ),
         last_value(
-          [:osmon, :cpu_avg1],
+          @prefix ++ [:osmon, :cpu_avg1],
           event_name: @event_cpu_la,
           description: "The average system load in the last minute.",
           measurement: :avg1
         ),
         last_value(
-          [:osmon, :cpu_avg5],
+          @prefix ++ [:osmon, :cpu_avg5],
           event_name: @event_cpu_la,
           description: "The average system load in the last five minutes.",
           measurement: :avg5
         ),
         last_value(
-          [:osmon, :cpu_avg15],
+          @prefix ++ [:osmon, :cpu_avg15],
           event_name: @event_cpu_la,
           description: "The average system load in the last 15 minutes.",
           measurement: :avg15
