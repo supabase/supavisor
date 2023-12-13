04b6092bd9ee85ac9ebe27b4ad04e407fe695d10
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2023-12-13 12:56:15 -0600

    fix: create roles in test db as well

--- priv/repo/seeds_before_migration.exs
+++ priv/repo/seeds_before_migration.exs
@@ -1,6 +1,9 @@
 import Ecto.Adapters.SQL, only: [query: 3]
 
 [
-  "create schema if not exists _supavisor"
+  "create schema if not exists _supavisor",
+  "create role anon          nologin noinherit;",
+  "create role authenticated nologin noinherit;",
+  "create role service_role  nologin noinherit bypassrls;",
 ]
 |> Enum.each(&query(Supavisor.Repo, &1, []))
