3b07af03d406387cd9b8ba21685c01eb94960374
Stas <sts@abc3.dev>
2023-12-18 20:42:45 +0100

    remove psql from tests

---
+++ test/support/fixtures/single_connection.ex
@@ -0,0 +1,34 @@
+defmodule SingleConnection do
+  alias Postgrex, as: P
+  @behaviour P.SimpleConnection
+
+  def connect(conf) do
+    P.SimpleConnection.start_link(__MODULE__, [], conf)
+  end
+
+  @impl true
+  def init(_args) do
+    {:ok, %{from: nil}}
+  end
+
+  @impl true
+  def handle_call({:query, query}, from, state) do
+    {:query, query, %{state | from: from}}
+  end
+
+  def handle_result(results, state) when is_list(results) do
+    IO.inspect({213, results})
+    P.SimpleConnection.reply(state.from, results)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_result(%Postgrex.Error{} = error, state) do
+    IO.inspect({213, error})
+    P.SimpleConnection.reply(state.from, error)
+    {:noreply, state}
+  end
+
+  @impl true
+  def notify(_, _, _), do: :ok
+end
