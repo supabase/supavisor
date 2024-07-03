ddb5fa3c989be028efc5f0ae88b3d024eb09860d
Stas <sts@abc3.dev>
2024-07-03 17:26:06 +0200

    *

--- lib/supavisor/db_handler.ex
+++ lib/supavisor/db_handler.ex
@@ -87,7 +87,7 @@ defmodule Supavisor.DbHandler do
       :binary,
       {:packet, :raw},
       {:active, false},
-      # {:nodelay, true},
+      {:nodelay, true},
       auth.ip_version
     ]
 
