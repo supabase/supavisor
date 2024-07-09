699f9ff8b55ed6a2642bfffe3682bccf5d666b36
Łukasz Jan Niemier <lukasz@niemier.pl>
2024-07-09 09:54:38 +0200

    ft: remove all HTML-related functionalities (#392)
    Supavisor is not using any HTML views, it do not offer any HTML-based
    UI, etc. so these functionalities aren't needed for anything.

--- mix.exs
+++ mix.exs
@@ -42,7 +42,6 @@ defmodule Supavisor.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
       {:phoenix_view, "~> 2.0.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.0"},
