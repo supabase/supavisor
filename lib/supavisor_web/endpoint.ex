43dd1fcad02ad9e77201cd4fcb6b6958bf5c994a
Łukasz Jan Niemier <lukasz@supabase.io>
2024-11-15 12:06:06 +0100

    chore: remove unneeded Plugs (#484)
    These plugs weren't used at all, as we do not serve any static assets nor we
    need live reload for anything (as we do not have any form of web UI).

--- lib/supavisor_web/endpoint.ex
+++ lib/supavisor_web/endpoint.ex
@@ -12,24 +12,6 @@ defmodule SupavisorWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :supavisor,
-    gzip: false,
-    only: ~w(assets fonts images favicon-32x32.png robots.txt)
-
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
-  if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
-  end
-
   plug Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
