3450c71ee6b317d21efed1dbe814629dbd454642
Chase Granberry <chase@logflare.app>
2024-11-25 21:29:04 -0700

    fix: cleanup

--- lib/supavisor/client_handler.ex
+++ lib/supavisor/client_handler.ex
@@ -384,6 +384,9 @@ defmodule Supavisor.ClientHandler do
   def handle_event(:internal, :subscribe, _, data) do
     Logger.debug("ClientHandler: Subscribe to tenant #{inspect(data.id)}")
 
+    manager_ref = Process.monitor(opts.workers.manager)
+    data = %{data | manager: manager_ref}
+
     with {:ok, sup} <-
            Supavisor.start_dist(data.id, data.auth_secrets,
              log_level: data.log_level,
@@ -395,10 +398,9 @@ defmodule Supavisor.ClientHandler do
              else: true
            ),
          {:ok, opts} <- Supavisor.subscribe(sup, data.id) do
-      manager_ref = Process.monitor(opts.workers.manager)
       data = Map.merge(data, opts.workers)
       db_pid = db_checkout(:both, :on_connect, data)
-      data = %{data | manager: manager_ref, db_pid: db_pid, idle_timeout: opts.idle_timeout}
+      data = %{data | db_pid: db_pid, idle_timeout: opts.idle_timeout}
 
       next =
         if opts.ps == [],
@@ -424,8 +426,6 @@ defmodule Supavisor.ClientHandler do
       :proxy ->
         case Supavisor.get_pool_ranch(data.id) do
           {:ok, %{port: port, host: host}} ->
-            manager_ref = Process.monitor(opts.workers.manager)
-
             auth =
               Map.merge(data.auth, %{
                 port: port,
@@ -436,8 +436,7 @@ defmodule Supavisor.ClientHandler do
                 upstream_verify: nil
               })
 
-            {:keep_state, %{data | auth: auth, manager: manager_ref},
-             {:next_event, :internal, :connect_db}}
+            {:keep_state, %{data | auth: auth}, {:next_event, :internal, :connect_db}}
 
           other ->
             Logger.error("ClientHandler: Subscribe proxy error: #{inspect(other)}")
