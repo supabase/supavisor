1b19f0fca6b3b4ba58db20f164e2677ac9073619
Łukasz Jan Niemier <lukasz@supabase.io>
2025-01-13 15:01:12 +0100

    fix: use `data` instead of `state` when handling unknown messages

--- lib/supavisor/client_handler.ex
+++ lib/supavisor/client_handler.ex
@@ -604,7 +604,7 @@ defmodule Supavisor.ClientHandler do
     msg = [
       {"type", type},
       {"content", content},
-      {"state", Map.put(state, :auth_secrets, "<REDACTED>")},
+      {"state", Map.put(data, :auth_secrets, "<REDACTED>")},
       {"data", data}
     ]
 
