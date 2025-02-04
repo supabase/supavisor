f67ffe710d0b7804be5118ebe12b74056d9b4f97
Łukasz Jan Niemier <lukasz@supabase.io>
2025-02-04 12:20:51 +0100

    test: do not fix version in migrations
    This provides more streamlined testing behaviour, as it will not
    randomly fail when the DB version changes.

--- priv/repo/seeds_after_migration.exs
+++ priv/repo/seeds_after_migration.exs
@@ -10,82 +10,78 @@ if Tenants.get_tenant_by_external_id(tenant_name) do
   Tenants.delete_tenant_by_external_id(tenant_name)
 end
 
-{:ok, version} =
-  case Repo.query!("select version()") do
-    %{rows: [[ver]]} -> Supavisor.Helpers.parse_pg_version(ver)
-    _ -> nil
-  end
-
 if !Tenants.get_tenant_by_external_id("is_manager") do
-  %{
-    db_host: db_conf[:hostname],
-    db_port: db_conf[:port],
-    db_database: db_conf[:database],
-    external_id: "is_manager",
-    default_parameter_status: %{server_version: version},
-    require_user: false,
-    auth_query: "SELECT rolname, rolpassword FROM pg_authid WHERE rolname=$1;",
-    users: [
-      %{
-        "db_user" => db_conf[:username],
-        "db_password" => db_conf[:password],
-        "pool_size" => 2,
-        "mode_type" => "transaction",
-        "is_manager" => true,
-        "pool_checkout_timeout" => 1000
-      }
-    ]
-  }
-  |> Tenants.create_tenant()
+  {:ok, _} =
+    %{
+      db_host: db_conf[:hostname],
+      db_port: db_conf[:port],
+      db_database: db_conf[:database],
+      default_parameter_status: %{},
+      external_id: "is_manager",
+      require_user: false,
+      auth_query: "SELECT rolname, rolpassword FROM pg_authid WHERE rolname=$1;",
+      users: [
+        %{
+          "db_user" => db_conf[:username],
+          "db_password" => db_conf[:password],
+          "pool_size" => 2,
+          "mode_type" => "transaction",
+          "is_manager" => true,
+          "pool_checkout_timeout" => 1000
+        }
+      ]
+    }
+    |> Tenants.create_tenant()
 end
 
 ["proxy_tenant1", "syn_tenant", "prom_tenant", "max_pool_tenant"]
 |> Enum.each(fn tenant ->
   if !Tenants.get_tenant_by_external_id(tenant) do
-    %{
-      db_host: db_conf[:hostname],
-      db_port: db_conf[:port],
-      db_database: db_conf[:database],
-      external_id: tenant,
-      default_parameter_status: %{server_version: version},
-      require_user: true,
-      users: [
-        %{
-          "db_user" => db_conf[:username],
-          "db_password" => db_conf[:password],
-          "pool_size" => 9,
-          "max_clients" => 100,
-          "mode_type" => "transaction"
-        },
-        %{
-          "db_user_alias" => "transaction",
-          "db_user" => db_conf[:username],
-          "db_password" => db_conf[:password],
-          "pool_size" => 3,
-          "max_clients" => 100,
-          "mode_type" => "transaction"
-        },
-        %{
-          "db_user_alias" => "session",
-          "db_user" => db_conf[:username],
-          "db_password" => db_conf[:password],
-          "pool_size" => 1,
-          "mode_type" => "session",
-          "max_clients" => 100,
-          "pool_checkout_timeout" => 500
-        },
-        %{
-          "db_user_alias" => "max_clients",
-          "db_user" => db_conf[:username],
-          "db_password" => db_conf[:password],
-          "pool_size" => 1,
-          "max_clients" => -1,
-          "mode_type" => "transaction",
-          "pool_checkout_timeout" => 500
-        }
-      ]
-    }
-    |> Tenants.create_tenant()
+    {:ok, _} =
+      %{
+        db_host: db_conf[:hostname],
+        db_port: db_conf[:port],
+        db_database: db_conf[:database],
+        default_parameter_status: %{},
+        external_id: tenant,
+        require_user: true,
+        users: [
+          %{
+            "db_user" => db_conf[:username],
+            "db_password" => db_conf[:password],
+            "pool_size" => 9,
+            "max_clients" => 100,
+            "mode_type" => "transaction"
+          },
+          %{
+            "db_user_alias" => "transaction",
+            "db_user" => db_conf[:username],
+            "db_password" => db_conf[:password],
+            "pool_size" => 3,
+            "max_clients" => 100,
+            "mode_type" => "transaction"
+          },
+          %{
+            "db_user_alias" => "session",
+            "db_user" => db_conf[:username],
+            "db_password" => db_conf[:password],
+            "pool_size" => 1,
+            "mode_type" => "session",
+            "max_clients" => 100,
+            "pool_checkout_timeout" => 500
+          },
+          %{
+            "db_user_alias" => "max_clients",
+            "db_user" => db_conf[:username],
+            "db_password" => db_conf[:password],
+            "pool_size" => 1,
+            "max_clients" => -1,
+            "mode_type" => "transaction",
+            "pool_checkout_timeout" => 500
+          }
+        ]
+      }
+      |> Tenants.create_tenant()
   end
 end)
 
