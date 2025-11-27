4bef17582b1b86b1a89c9a8c91e707344e0fe6b0
felipe stival <v0idpwn@gmail.com>
2025-11-27 17:29:41 -0300

    chore: bump peep, remove dialyzer skips

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,9 +1,1 @@
-[
-  ~r(test/support/[^.]*\.ex),
-  # TODO: Remove these ignores once Peep library fixes typespec in https://github.com/rkallos/peep/pull/56
-  {"lib/supavisor/metrics_cleaner.ex", :no_return},
-  {"lib/supavisor/metrics_cleaner.ex", :call_with_opaque},
-  {"lib/supavisor/monitoring/prom_ex.ex", :no_return},
-  {"lib/supavisor/monitoring/prom_ex.ex", :call_with_opaque},
-  {"lib/supavisor/monitoring/prom_ex.ex", :unused_fun}
-]
+[]
--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Supavisor.MixProject do
       {:phoenix_live_view, "~> 1.0"},
       {:phoenix_live_dashboard, "~> 0.7"},
       {:telemetry_poller, "~> 1.0"},
-      {:peep, "~> 4.2", override: true},
+      {:peep, "~> 4.2 and >= 4.2.1", override: true},
       {:plug_cowboy, "~> 2.5"},
       {:joken, "~> 2.6.0"},
       {:cloak_ecto, "~> 1.3.0"},
