7ab06685522373b32ca189e1d1879b5cd9e07171
felipe stival <v0idpwn@gmail.com>
2026-02-09 13:45:58 +0700

    docs

--- lib/supavisor/secret.ex
+++ lib/supavisor/secret.ex
@@ -8,7 +8,18 @@ defmodule Supavisor.Secret do
 
   @type auth_method :: :password | :auth_query | :auth_query_md5
 
+  @doc """
+  Converts a secret struct into a map that can be encoded in JSON.
+  """
   @callback to_encodable_map(struct()) :: map()
+
+  @doc """
+  Converts a map decoded from JSON into a secret struct.
+  """
   @callback from_decoded_map(map()) :: struct()
+
+  @doc """
+  Returns the authentication method associated with the secret type.
+  """
   @callback auth_method() :: auth_method()
 end
