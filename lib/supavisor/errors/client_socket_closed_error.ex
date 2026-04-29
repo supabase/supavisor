8ac772bf0490f573eb7746f75126a20cd14cfa83
felipe stival <v0idpwn@gmail.com>
2026-04-29 14:10:14 -0300

    fix: gracefully shutdown on client socket closed errors
    Avoids noisy runtime crashes.

--- lib/supavisor/errors/client_socket_closed_error.ex
+++ lib/supavisor/errors/client_socket_closed_error.ex
@@ -3,17 +3,25 @@ defmodule Supavisor.Errors.ClientSocketClosedError do
   This error is returned when a client socket is closed unexpectedly
   """
 
-  use Supavisor.Error, [:mode, :client_state, code: "ECLIENTSOCKETCLOSED"]
+  use Supavisor.Error, [:mode, :client_state, :reason, code: "ECLIENTSOCKETCLOSED"]
 
   @type t() :: %__MODULE__{
           mode: atom() | nil,
           client_state: atom() | nil,
+          reason: term() | nil,
           code: binary()
         }
 
   @impl Supavisor.Error
   def error_message(%{mode: mode, client_state: client_state}) do
     "Client socket closed while state was #{client_state} (#{mode})"
+  end
+
+  @impl Supavisor.Error
+  def log_message(%{reason: nil} = error), do: message(error)
+
+  def log_message(%{reason: reason} = error) do
+    "#{message(error)}: #{inspect(reason)}"
   end
 
   @impl Supavisor.Error
