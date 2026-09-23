41e3a53951e17000dc04f86e5d4a1acfe57ad279
felipe stival <v0idpwn@gmail.com>
2026-09-23 09:44:59 -0300

    fix merge conflict

--- test/supavisor/client_handler_test.exs
+++ test/supavisor/client_handler_test.exs
@@ -201,6 +201,7 @@ defmodule Supavisor.ClientHandlerTest do
         mode: :transaction,
         db_connection: {:pool, self(), {:gen_tcp, db_sock}},
         tenant_feature_flags: %{},
+        txn_mode_leak_action: :ignore,
         stream_state: MessageStreamer.new_stream_state(FrontendMessageHandler)
       }
 
@@ -223,6 +224,7 @@ defmodule Supavisor.ClientHandlerTest do
         mode: :transaction,
         db_connection: {:pool, self(), {:gen_tcp, db_sock}},
         tenant_feature_flags: %{},
+        txn_mode_leak_action: :ignore,
         stream_state: MessageStreamer.new_stream_state(FrontendMessageHandler)
       }
 
